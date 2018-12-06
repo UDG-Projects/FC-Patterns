bebbca3ffe1f50371e977bf1f06d3f846dfc14d5
gil gasso <gilgr1990@gmail.com>
2018-12-06 15:19:17 +0100

    using clion gil

---
+++ cmake-build-debug/CMakeFiles/TuringMachine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TuringMachine.dir/main.cpp.obj"
+  "CMakeFiles/TuringMachine.dir/Matrix.cpp.obj"
+  "TuringMachine.pdb"
+  "TuringMachine.exe"
+  "TuringMachine.exe.manifest"
+  "libTuringMachine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TuringMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
